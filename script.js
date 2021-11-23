2074e1f7d516be87892db9f1b89f7157e14182b6
Mason <mt9260@gmail.com>
2021-11-22 23:28:04 -0600

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        return response.json()
+    }).then(function(json){
+        console.log(json)
+        let rocketman = ""
+        let container = document.querySelector("#container");
+        console.log(container)
+        for(astro of json){
+            rocketman += `
+            <div class="astronaut">
+            <div class="bio">
+            <h3>${astro.firstName} ${astro.lastName}</h3>
+            <ul>
+                <li>hours in space: ${astro.hoursInSpace}</li>
+                <li>Active: ${astro.active}</li>
+                <li>Skills: ${astro.skills.join(", ")}</li>         
+            </ul>
+        </div>
+        <img class="avatar" src="${astro.picture}"/>
+        </div>`
+        }
+        container.innerHTML= rocketman
+    })
+})
